9600f554fcd70abffff5f2065f598060f45e31fe
Shaoting Wu <alice.wu.nz@gmail.com>
2016-02-09 13:14:16 +1300

    Add columns to the users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username',20)->unique();
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->string('profileImage',30);
+            $table->string('description',150);
+            $table->string('website',100);
+            $table->string('background',30);
             $table->rememberToken();
             $table->timestamps();
         });
