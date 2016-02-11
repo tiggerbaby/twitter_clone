041ff82e7c25ddbb56d39f8ab40923b487af7f5d
Shaoting Wu <alice.wu.nz@gmail.com>
2016-02-12 12:40:32 +1300

    Add Image Intervention to the project

--- config/app.php
+++ config/app.php
@@ -126,6 +126,9 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+
+        Intervention\Image\ImageServiceProvider::class,
+
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -171,6 +174,8 @@ return [
 
     'aliases' => [
 
+        'Image' => Intervention\Image\Facades\Image::class,
+        
         'App'       => Illuminate\Support\Facades\App::class,
         'Artisan'   => Illuminate\Support\Facades\Artisan::class,
         'Auth'      => Illuminate\Support\Facades\Auth::class,
